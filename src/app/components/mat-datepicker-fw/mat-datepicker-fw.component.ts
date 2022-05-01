ad90dd6651b94085d1937743bd49ab7f127e1597
JayPi89 <56943962+JayPi89@users.noreply.github.com>
2022-05-01 12:08:05 +0200

    save input

--- src/app/components/mat-datepicker-fw/mat-datepicker-fw.component.ts
+++ src/app/components/mat-datepicker-fw/mat-datepicker-fw.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Output, Input} from '@angular/core';
+import {FormControl, FormGroup, FormGroupDirective} from '@angular/forms';
 import {MomentDateAdapter} from '@angular/material-moment-adapter';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
@@ -10,16 +11,15 @@ export class CustomizedDateAdapter extends MomentDateAdapter {
 
 export const MY_FORMATS = {
   parse: {
-    dateInput: 'DD.MM.YYYY',
+    dateInput: 'DD.MM.YYYY HH:mm',
   },
   display: {
-    dateInput: 'DD.MM.YYYY',
+    dateInput: 'DD.MM.YYYY HH:mm',
     monthYearLabel: 'YYYY',
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'YYYY',
   },
 };
- 
 
 @Component({
   selector: 'app-mat-datepicker-fw',
@@ -30,19 +30,114 @@ export const MY_FORMATS = {
     {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
   ],
 })
-export class MatDatepickerFwComponent implements OnInit {
+export class MatDatepickerFwComponent {
 
-  formatLabel: any;
-  clicked = false;
-  selected: Date | null | undefined;
+  @Input() isRange = true;
+  @Input() type: 'range' | 'start' | 'end' | undefined;
 
-  constructor() { }
+  beginTime = new Date();
+  endTime = new Date();
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  stH = 0;
+  enM = 0;
+  stM = 0;
+  enH = 0;
 
-  ngOnInit(): void {
+  constructor() {
+    // was beim neu öffnen ohne änderungen am datum aber an der uhrzeit???
+    this.initDefaultTime();
   }
 
-  onTimeChange(x: any) {
+  onSubmit1() {
+    try {
+      const start = this.range.get('start')?.value.toDate() as Date;
+      start.setHours(this.beginTime.getHours());
+      start.setMinutes(this.beginTime.getMinutes());
+      this.range.get('start')?.setValue(start);
+      const end = this.range.get('end')?.value.toDate() as Date;
+      end.setHours(this.endTime.getHours());
+      end.setMinutes(this.endTime.getMinutes());
+      this.range.get('end')?.setValue(end);
+    } catch (error) { }
+  }
 
+  initDefaultTime() {
+    this.beginTime.setHours(8);
+    this.beginTime.setMinutes(0);
+    this.endTime.setHours(18);
+    this.endTime.setMinutes(0);
+  }
+
+  addMin() {
+    if (this.beginTime.getMinutes() >= 45) {
+      this.beginTime = new Date(this.beginTime);
+      this.beginTime.setHours(this.beginTime.getHours() + 1);
+      this.beginTime.setMinutes(0);
+      return;
+    }
+    this.beginTime = new Date(this.beginTime);
+    this.beginTime.setMinutes(this.beginTime.getMinutes() + 15);
+  }
+
+  removeMin() {
+    if (this.beginTime.getMinutes() <= 0) {
+      this.beginTime = new Date(this.beginTime);
+      this.beginTime.setHours(this.beginTime.getHours() - 1);
+      this.beginTime.setMinutes(45);
+      return;
+    }
+    this.beginTime = new Date(this.beginTime);
+    this.beginTime.setMinutes(this.beginTime.getMinutes() - 15);
+  }
+
+  setMin(value: number) {
+    this.beginTime = new Date(this.beginTime);
+    this.beginTime.setMinutes(value);
+  }
+
+  setHour(value: number) {
+    this.beginTime = new Date(this.beginTime);
+    this.beginTime.setHours(value);
+  }
+
+  addMinEnd() {
+    if (this.endTime.getMinutes() >= 45) {
+      this.endTime = new Date(this.endTime);
+      this.endTime.setHours(this.endTime.getHours() + 1);
+      this.endTime.setMinutes(0);
+      return;
+    }
+    this.endTime = new Date(this.endTime);
+    this.endTime.setMinutes(this.endTime.getMinutes() + 15);
+  }
+
+  removeMinEnd() {
+    if (this.endTime.getMinutes() <= 0) {
+      this.endTime = new Date(this.endTime);
+      this.endTime.setHours(this.endTime.getHours() - 1);
+      this.endTime.setMinutes(45);
+      return;
+    }
+    this.endTime = new Date(this.endTime);
+    this.endTime.setMinutes(this.endTime.getMinutes() - 15);
+  }
+
+  setMinEnd(value: number) {
+    this.endTime = new Date(this.endTime);
+    this.endTime.setMinutes(value);
+  }
+
+  setHourEnd(value: number) {
+    this.endTime = new Date(this.endTime);
+    this.endTime.setHours(value);
+  }
+
+  refreshForm(formDirective: FormGroupDirective) {
+    formDirective.resetForm();
+    //this.myForm = this.createMyForm();
   }
 
 }
